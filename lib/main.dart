de82e52b11d9eadde1f00840c71fc64e95d2c4bf
ColoApp <coloapp.anuar.gu@gmail.com>
2025-04-05 14:40:25 -0600

    nueva primera version SCRUM

--- lib/main.dart
+++ lib/main.dart
@@ -165,127 +165,165 @@ class SolicitudFormState extends State<SolicitudForm> {
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.start,
           children: <Widget>[
-            const Text("Datos Personales", style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold)),
-            TextFormField(
-              controller: _apellidoPaternoController,
-              decoration: const InputDecoration(labelText: 'Primer Apellido'),
-            ),
-            TextFormField(
-              controller: _apellidoMaternoController,
-              decoration: const InputDecoration(labelText: 'Segundo Apellido'),
-            ),
-            TextFormField(
-              controller: _nombreController,
-              decoration: const InputDecoration(labelText: 'Nombre(s)'),
-              validator: (value) {
-                if (value == null || value.isEmpty) {
-                  return 'Por favor ingresa tu nombre';
-                }
-                return null;
-              },
-            ),
-            TextFormField(
-              controller: _edadController,
-              decoration: const InputDecoration(labelText: 'Edad (años)'),
-            ),
-            TextFormField(
-              controller: _domicilioController,
-              decoration: const InputDecoration(labelText: 'Domicilio (Calle y Número)'),
-            ),
-            TextFormField(
-              controller: _coloniaController,
-              decoration: const InputDecoration(labelText: 'Colonia'),
-            ),
-            TextFormField(
-              controller: _codigoPostalController,
-              decoration: const InputDecoration(labelText: 'Código Postal'),
-            ),
-            TextFormField(
-              controller: _telefonoController,
-              decoration: const InputDecoration(labelText: 'Teléfono o Celular'),
-              keyboardType: TextInputType.phone,
-              validator: (value) {
-                if (value != null && value.isNotEmpty) {
-                  if (!RegExp(r'^[0-9]{10}$').hasMatch(value)) {
-                    return 'Ingresa un número de 10 dígitos';
-                  }
-                }
-                return null;
-              },
-            ),
-            TextFormField(
-              controller: _sexoController,
-              decoration: const InputDecoration(labelText: 'Sexo (Masculino o Femenino)'),
-            ),
-            TextFormField(
-              controller: _ciudadController,
-              decoration: const InputDecoration(labelText: 'Ciudad o Alcaldía'),
-            ),
-            TextFormField(
-              controller: _entidadFederativaController,
-              decoration: const InputDecoration(labelText: 'Entidad Federativa del Domicilio'),
-            ),
-            TextFormField(
-              controller: _lugarNacimientoController,
-              decoration: const InputDecoration(labelText: 'Entidad Federativa (lugar) de Nacimiento'),
-            ),
-            _buildFechaNacimientoField(),
-            TextFormField(
-              controller: _nacionalidadController,
-              decoration: const InputDecoration(labelText: 'Nacionalidad'),
-            ),
-            TextFormField(
-              controller: _viveConController,
-              decoration: const InputDecoration(labelText: 'Vive con (Padres, Familia, Parientes)'),
-            ),
-            TextFormField(
-              controller: _dependientesController,
-              decoration: const InputDecoration(labelText: 'Personas que dependen de usted (Hijos, Cónyuge, Padres)'),
-            ),
-            TextFormField(
-              controller: _estadoCivilController,
-              decoration: const InputDecoration(labelText: 'Estado Civil'),
-            ),
-            const Text("DOCUMENTACIÓN", style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold)),
-            TextFormField(
-              controller: _curpController,
-              decoration: const InputDecoration(labelText: 'Clave Única de Registro de Población (CURP)'),
-            ),
-            TextFormField(
-              controller: _aforeController,
-              decoration: const InputDecoration(labelText: 'No de AFORE'),
-            ),
-            TextFormField(
-              controller: _rfcController,
-              decoration: const InputDecoration(labelText: 'Registro Federal de Contribuyentes (RFC)'),
-            ),
-            TextFormField(
-              controller: _licenciaManejoController,
-              decoration: const InputDecoration(labelText: '¿Tiene Licencia de Manejo? (Si o No)'),
-            ),
-            TextFormField(
-              controller: _cartillaMilitarController,
-              decoration: const InputDecoration(labelText: 'Cartilla de Servicio Militar No.'),
-            ),
-            TextFormField(
-              controller: _pasaporteController,
-              decoration: const InputDecoration(labelText: 'No. Pasaporte'),
-            ),
-            TextFormField(
-              controller: _nssController,
-              decoration: const InputDecoration(labelText: 'Número de Seguridad Social'),
-            ),
-            TextFormField(
-              controller: _licenciaClaseNumeroController,
-              decoration: const InputDecoration(labelText: 'Clase y número de licencia'),
-            ),
-            TextFormField(
-              controller: _extranjeroController,
-              decoration: const InputDecoration(labelText: 'Siendo extranjero:'),
-            ),
+            if (_currentSection == 1) ..._buildSeccionDatosPersonales(),
+            if (_currentSection == 2) ..._buildSeccionEscolaridad(),
+            if (_currentSection == 3) ..._buildSeccionExperienciaLaboral(),
+            if (_currentSection == 4) ..._buildSeccionReferencias(),
           ],
         ),
       ),
+    );
+  }
+
+  List<Widget> _buildSeccionDatosPersonales() {
+    return [
+      const Text("Datos Personales", style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold)),
+      TextFormField(controller: _apellidoPaternoController, decoration: const InputDecoration(labelText: 'Primer Apellido')),
+      TextFormField(controller: _apellidoMaternoController, decoration: const InputDecoration(labelText: 'Segundo Apellido')),
+      TextFormField(controller: _nombreController, decoration: const InputDecoration(labelText: 'Nombre(s)')),
+      TextFormField(controller: _edadController, decoration: const InputDecoration(labelText: 'Edad (años)')),
+      TextFormField(controller: _domicilioController, decoration: const InputDecoration(labelText: 'Domicilio (Calle y Número)')),
+      TextFormField(controller: _coloniaController, decoration: const InputDecoration(labelText: 'Colonia')),
+      TextFormField(controller: _codigoPostalController, decoration: const InputDecoration(labelText: 'Código Postal')),
+      TextFormField(controller: _telefonoController, decoration: const InputDecoration(labelText: 'Teléfono o Celular'), keyboardType: TextInputType.phone),
+      TextFormField(controller: _sexoController, decoration: const InputDecoration(labelText: 'Sexo (Masculino o Femenino)')),
+      TextFormField(controller: _ciudadController, decoration: const InputDecoration(labelText: 'Ciudad o Alcaldía')),
+      TextFormField(controller: _entidadFederativaController, decoration: const InputDecoration(labelText: 'Entidad Federativa del Domicilio')),
+      TextFormField(controller: _lugarNacimientoController, decoration: const InputDecoration(labelText: 'Entidad Federativa (lugar) de Nacimiento')),
+      _buildFechaNacimientoField(),
+      TextFormField(controller: _nacionalidadController, decoration: const InputDecoration(labelText: 'Nacionalidad')),
+      TextFormField(controller: _viveConController, decoration: const InputDecoration(labelText: 'Vive con (Padres, Familia, Parientes)')),
+      TextFormField(controller: _dependientesController, decoration: const InputDecoration(labelText: 'Personas que dependen de usted (Hijos, Cónyuge, Padres)')),
+      TextFormField(controller: _estadoCivilController, decoration: const InputDecoration(labelText: 'Estado Civil')),
+      const Text("DOCUMENTACIÓN", style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold)),
+      TextFormField(controller: _curpController, decoration: const InputDecoration(labelText: 'Clave Única de Registro de Población (CURP)')),
+      TextFormField(controller: _aforeController, decoration: const InputDecoration(labelText: 'No de AFORE')),
+      TextFormField(controller: _rfcController, decoration: const InputDecoration(labelText: 'Registro Federal de Contribuyentes (RFC)')),
+      TextFormField(controller: _licenciaManejoController, decoration: const InputDecoration(labelText: '¿Tiene Licencia de Manejo? (Si o No)')),
+      TextFormField(controller: _cartillaMilitarController, decoration: const InputDecoration(labelText: 'Cartilla de Servicio Militar No.')),
+      TextFormField(controller: _pasaporteController, decoration: const InputDecoration(labelText: 'No. Pasaporte')),
+      TextFormField(controller: _nssController, decoration: const InputDecoration(labelText: 'Número de Seguridad Social')),
+      TextFormField(controller: _licenciaClaseNumeroController, decoration: const InputDecoration(labelText: 'Clase y número de licencia')),
+      TextFormField(controller: _extranjeroController, decoration: const InputDecoration(labelText: 'Siendo extranjero:')),
+    ];
+  }
+
+  List<Widget> _buildSeccionEscolaridad() {
+    return [
+      const Text("Escolaridad", style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold)),
+      _buildEscolaridadForm("Primaria"),
+      _buildEscolaridadForm("Secundaria"),
+      _buildEscolaridadForm("Preparatoria o Vocacional"),
+      _buildEscolaridadForm("Profesional"),
+      _buildEscolaridadForm("Comercial u otras"),
+      _buildEstudiosActualesForm(),
+    ];
+  }
+
+  Widget _buildEscolaridadForm(String nivel) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Text(nivel, style: const TextStyle(fontWeight: FontWeight.bold)),
+        TextFormField(decoration: const InputDecoration(labelText: 'Nombre de la escuela')),
+        TextFormField(decoration: const InputDecoration(labelText: 'Domicilio')),
+        Row(
+          children: [
+            Expanded(child: TextFormField(decoration: const InputDecoration(labelText: 'De'))),
+            const SizedBox(width: 10),
+            Expanded(child: TextFormField(decoration: const InputDecoration(labelText: 'A'))),
+          ],
+        ),
+        TextFormField(decoration: const InputDecoration(labelText: 'Años')),
+        TextFormField(decoration: const InputDecoration(labelText: 'Título Recibido')),
+        const SizedBox(height: 10),
+      ],
+    );
+  }
+
+  Widget _buildEstudiosActualesForm() {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        const Text("Estudios que está efectuando en la actualidad", style: TextStyle(fontWeight: FontWeight.bold)),
+        TextFormField(decoration: const InputDecoration(labelText: 'Escuela')),
+        TextFormField(decoration: const InputDecoration(labelText: 'Horario')),
+        TextFormField(decoration: const InputDecoration(labelText: 'Curso o carrera')),
+        TextFormField(decoration: const InputDecoration(labelText: 'Grado')),
+      ],
+    );
+  }
+
+  List<Widget> _buildSeccionExperienciaLaboral() {
+    return [
+      const Text("Empleo Actual o Anteriores", style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold)),
+      _buildEmpleoForm("Empleo actual o último"),
+      _buildEmpleoForm("Empleo anterior"),
+      _buildEmpleoForm("Empleo anterior"),
+      Row(
+        children: [
+          const Text("Podemos solicitar informes de usted?"),
+          Radio(value: true, groupValue: null, onChanged: (value) {}),
+          const Text("Sí"),
+          Radio(value: false, groupValue: null, onChanged: (value) {}),
+          const Text("No (Razones)"),
+        ],
+      ),
+    ];
+  }
+
+  Widget _buildEmpleoForm(String titulo) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Text(titulo, style: const TextStyle(fontWeight: FontWeight.bold)),
+        TextFormField(decoration: const InputDecoration(labelText: 'Tiempo que prestó sus servicios')),
+        TextFormField(decoration: const InputDecoration(labelText: 'Nombre de la Empresa')),
+        TextFormField(decoration: const InputDecoration(labelText: 'Domicilio')),
+        TextFormField(decoration: const InputDecoration(labelText: 'Teléfono')),
+        TextFormField(decoration: const InputDecoration(labelText: 'Puesto')),
+        Row(
+          children: [
+            Expanded(child: TextFormField(decoration: const InputDecoration(labelText: 'Inicial'))),
+            const SizedBox(width: 10),
+            Expanded(child: TextFormField(decoration: const InputDecoration(labelText: 'Final'))),
+          ],
+        ),
+        Row(
+          children: [
+            Expanded(child: TextFormField(decoration: const InputDecoration(labelText: 'Sueldos Inicial'))),
+            const SizedBox(width: 10),
+            Expanded(child: TextFormField(decoration: const InputDecoration(labelText: 'Sueldos Final'))),
+          ],
+        ),
+        TextFormField(decoration: const InputDecoration(labelText: 'Motivos de su separación')),
+        TextFormField(decoration: const InputDecoration(labelText: 'Nombre de su jefe inmediato')),
+        TextFormField(decoration: const InputDecoration(labelText: 'Actividades Desempeñadas')),
+        const SizedBox(height: 10),
+      ],
+    );
+  }
+
+  List<Widget> _buildSeccionReferencias() {
+    return [
+      const Text("Referencias Personales", style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold)),
+      _buildReferenciaForm(),
+      _buildReferenciaForm(),
+      _buildReferenciaForm(),
+    ];
+  }
+
+  Widget _buildReferenciaForm() {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        TextFormField(decoration: const InputDecoration(labelText: 'Nombre')),
+        TextFormField(decoration: const InputDecoration(labelText: 'Domicilio')),
+        TextFormField(decoration: const InputDecoration(labelText: 'Teléfono')),
+        TextFormField(decoration: const InputDecoration(labelText: 'Ocupación')),
+        TextFormField(decoration: const InputDecoration(labelText: 'Tiempo de conocerse')),
+        const SizedBox(height: 10),
+      ],
     );
   }
 
